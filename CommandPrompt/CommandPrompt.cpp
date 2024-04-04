5a7dccf7769d79502d2e86c7df3b381f85d3c192
pha-ran <pha-ran@naver.com>
2024-04-04 14:32:09 +0900

    명령 프롬프트 프로젝트 수정

--- CommandPrompt/CommandPrompt.cpp
+++ CommandPrompt/CommandPrompt.cpp
@@ -1,1 +1,20 @@
-﻿
+﻿/*
+	명령 프롬프트 프로젝트
+*/
+
+#include <stdio.h>
+#include <windows.h>
+#include <tchar.h>
+#include <locale.h>
+
+#define MAX_STR_LEN 400
+#define MAX_TOK_NUM 200
+
+int _tmain(void)
+{
+	_wsetlocale(LC_ALL, L"korean");
+
+	
+
+	return 0;
+}
