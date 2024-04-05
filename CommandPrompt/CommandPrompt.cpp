287cc49ac689156a80eccac2d95dd5454aca726c
pha-ran <pha-ran@naver.com>
2024-04-05 14:55:21 +0900

    명령 프롬프트 프로젝트 수정

--- CommandPrompt/CommandPrompt.cpp
+++ CommandPrompt/CommandPrompt.cpp
@@ -15,21 +15,23 @@
 
 int ___istupper(_TINT c);
 int ___istlower(_TINT c);
+int ___totupper(_TINT c);
+int ___totlower(_TINT c);
 
 int _tmain(void)
 {
 	_wsetlocale(LC_ALL, L"korean");
 
-	_tprintf(_T("문자 크기 : %d\n\n"), sizeof(TCHAR));
-
+	_tprintf(_T("명령 프롬프트 프로젝트 ( %zd 바이트 시스템 / 문자 크기 %zd )\n\n"), sizeof(SIZE_T), sizeof(TCHAR));
+	
 	TCHAR str[] = _T("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789가나다라마바사아자차카타파하!@#$%^&*() \t\n,./+-<>;");
 
-	for (_TINT i = 0; str[i] != NULL; i++)
+	for (int i = 0; str[i] != NULL; i++)
 	{
 		_tprintf(_T("%c\n"), str[i]);
 
-		_tprintf(_T("대문자 여부 %d %d\n"), _istupper(str[i]), ___istupper(str[i]));
-		_tprintf(_T("소문자 여부 %d %d\n"), _istlower(str[i]), ___istlower(str[i]));
+		_tprintf(_T("대문자 변환 %c %c\n"), _totupper(str[i]), ___totupper(str[i]));
+		_tprintf(_T("소문자 변환 %c %c\n"), _totlower(str[i]), ___totlower(str[i]));
 
 		_tprintf(_T("\n"));
 	}
@@ -51,4 +53,26 @@ int ___istlower(_TINT c)
 		return 1;
 
 	return 0;
+}
+
+int ___totupper(_TINT c)
+{
+	if (___istlower(c))
+	{
+		int temp = _T('a') - _T('A');
+		c -= temp;
+	}
+
+	return c;
+}
+
+int ___totlower(_TINT c)
+{
+	if (___istupper(c))
+	{
+		int temp = _T('a') - _T('A');
+		c += temp;
+	}
+
+	return c;
 }
