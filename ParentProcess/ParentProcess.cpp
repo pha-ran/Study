24917ebe294351903fa37f86126334783850085e
pha-ran <pha-ran@naver.com>
2024-04-10 22:34:25 +0900

    Parent Process 프로젝트 추가

---
+++ ParentProcess/ParentProcess.cpp
@@ -0,0 +1,46 @@
+﻿// Parent Process
+
+#include <stdio.h>
+#include <windows.h>
+#include <tchar.h>
+
+int _tmain(int argc, TCHAR* argv[])
+{
+	STARTUPINFO si;
+	PROCESS_INFORMATION pi;
+
+	SecureZeroMemory(&si, sizeof(si));
+	SecureZeroMemory(&pi, sizeof(pi));
+
+	si.cb = sizeof(si);
+	si.dwX = 100;
+	si.dwY = 200;
+	si.dwXSize = 300;
+	si.dwYSize = 200;
+	si.dwFlags = STARTF_USEPOSITION | STARTF_USESIZE;
+
+	TCHAR commandLine[] = _T("ChildProcess.exe CreateProcess");
+
+	if (!CreateProcess(
+		NULL,
+		commandLine,
+		NULL,
+		NULL,
+		FALSE,
+		CREATE_NEW_CONSOLE,
+		NULL,
+		NULL,
+		&si,
+		&pi))
+	{
+		printf("CreateProcess failed (%d).\n", GetLastError());
+		return -1;
+	}
+
+	WaitForSingleObject(pi.hProcess, INFINITE);
+
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
+
+	return 0;
+}
