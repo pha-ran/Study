01654c31c6b7c6f9f81b415b5b345c277f060969
pha-ran <pha-ran@naver.com>
2024-04-10 15:27:24 +0900

    Child Process 프로젝트 추가

---
+++ ChildProcess/ChildProcess.cpp
@@ -0,0 +1,15 @@
+﻿// Child Process
+
+#include <stdio.h>
+#include <windows.h>
+#include <tchar.h>
+
+int _tmain(int argc, TCHAR* argv[])
+{
+	_tprintf(_T("%s\n"), argv[1]);
+	
+	TCHAR temp;
+	_tscanf_s(_T("%c"), &temp, (unsigned int)sizeof(temp));
+
+	return 0;
+}
