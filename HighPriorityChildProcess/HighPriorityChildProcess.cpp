0dd95f6232c2216c619e1df14be59c9ef13368e8
pha-ran <pha-ran@naver.com>
2024-04-12 12:05:06 +0900

    High Priority Child Process 프로젝트 추가

---
+++ HighPriorityChildProcess/HighPriorityChildProcess.cpp
@@ -0,0 +1,21 @@
+﻿// High Priority Child Process
+
+#include <windows.h>
+#include <tchar.h>
+#include <stdio.h>
+
+int _tmain(void)
+{
+	SetPriorityClass(GetCurrentProcess(), HIGH_PRIORITY_CLASS);
+
+	while (1)
+	{
+		for (int i = 0; i < 10000; i++)
+		{
+			for (int j = 0; j < 10000; j++)
+				_tprintf(_T("Child\n"));
+		}
+	}
+
+	return 0;
+}
