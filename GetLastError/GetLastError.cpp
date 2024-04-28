f6158e18637bb8148a163be3829a4545c8a393bf
pha-ran <pha-ran@naver.com>
2024-04-29 00:06:52 +0900

    GetLastError 프로젝트 추가

---
+++ GetLastError/GetLastError.cpp
@@ -0,0 +1,15 @@
+﻿#include <windows.h>
+#include <tchar.h>
+#include <stdio.h>
+
+int _tmain(void)
+{
+	TCHAR CurrentDirectory[MAX_PATH];
+	GetCurrentDirectory(MAX_PATH, CurrentDirectory);
+	_tprintf(_T("error code %d \n"), GetLastError());
+
+	SetCurrentDirectory(NULL);
+	_tprintf(_T("error code %d \n"), GetLastError());
+
+	return 0;
+}
