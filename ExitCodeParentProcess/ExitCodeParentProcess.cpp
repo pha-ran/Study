ff7775528ea45b17db2b25868df5679aae8f2150
pha-ran <pha-ran@naver.com>
2024-05-01 13:46:42 +0900

    Exit Code Parent Process 프로젝트 추가

---
+++ ExitCodeParentProcess/ExitCodeParentProcess.cpp
@@ -0,0 +1,36 @@
+﻿// Exit Code Parent Process
+
+#include <windows.h>
+#include <tchar.h>
+#include <stdio.h>
+
+int _tmain(void)
+{
+	STARTUPINFO si;
+	PROCESS_INFORMATION pi;
+	DWORD code;
+
+	SecureZeroMemory(&si, sizeof(si));
+	SecureZeroMemory(&pi, sizeof(pi));
+
+	si.cb = sizeof(si);
+
+	TCHAR commandLine[] = _T("ExitCodeChildProcess.exe");
+
+	if (!CreateProcess(NULL, commandLine, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi))
+	{
+		_tprintf(_T("CreateProcess failed (%d).\n"), GetLastError());
+		return -1;
+	}
+
+	WaitForSingleObject(pi.hProcess, INFINITE);
+
+	GetExitCodeProcess(pi.hProcess, &code);
+
+	_tprintf(_T("Get Exit Code : %d\n"), code);
+
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
+
+	return 0;
+}
