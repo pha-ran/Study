2287fb3d08033e929eba29a5da672c46650081cb
pha-ran <pha-ran@naver.com>
2024-05-01 12:16:55 +0900

    Close Handle 프로젝트 생성

--- CloseHandle/CloseHandle.cpp
+++ CloseHandle/CloseHandle.cpp
@@ -1,4 +1,4 @@
-﻿// Normal Priority Parent Process
+﻿// Close Handle
 
 #include <windows.h>
 #include <tchar.h>
@@ -22,26 +22,18 @@ int _tmain(void)
 		return -1;
 	}
 
-	DWORD count = 0;
+	SetPriorityClass(pi.hProcess, NORMAL_PRIORITY_CLASS);
 
-	while (1)
+	for (int i = 0; i < 10000; i++)
 	{
-		for (int i = 0; i < 10000; i++)
-		{
-			for (int j = 0; j < 10000; j++)
-				;
-		}
-
-		_tprintf(_T("Parent\n"));
-
-		count += 1;
-
-		if (count == 16)
-			SetPriorityClass(pi.hProcess, NORMAL_PRIORITY_CLASS);
+		for (int j = 0; j < 10000; j++)
+			;
 	}
 
 	CloseHandle(pi.hProcess);
 	CloseHandle(pi.hThread);
+
+	_tprintf(_T("Close Handle\n"));
 
 	return 0;
 }
