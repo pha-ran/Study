d4ec60b4589cad9e72fb9e4a169e850c19ee53f5
pha-ran <pha-ran@naver.com>
2024-05-07 19:48:35 +0900

    Adder Parent Process 프로젝트 추가

---
+++ AdderParentProcess/AdderParentProcess.cpp
@@ -0,0 +1,47 @@
+﻿// Adder Parent Process
+
+#include <windows.h>
+#include <tchar.h>
+#include <stdio.h>
+
+int _tmain(void)
+{
+	DWORD sum1;
+	DWORD sum2;
+
+	STARTUPINFO si;
+	PROCESS_INFORMATION pi;
+
+	// sum1
+	SecureZeroMemory(&si, sizeof(si));
+	SecureZeroMemory(&pi, sizeof(pi));
+	si.cb = sizeof(si);
+
+	TCHAR commandLine1[] = _T("AdderChildProcess.exe 1 5");
+	CreateProcess(NULL, commandLine1, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
+
+	WaitForSingleObject(pi.hProcess, INFINITE);
+	GetExitCodeProcess(pi.hProcess, &sum1);
+
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
+
+	// sum2
+	SecureZeroMemory(&si, sizeof(si));
+	SecureZeroMemory(&pi, sizeof(pi));
+	si.cb = sizeof(si);
+
+	TCHAR commandLine2[] = _T("AdderChildProcess.exe 6 10");
+	CreateProcess(NULL, commandLine2, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
+
+	WaitForSingleObject(pi.hProcess, INFINITE);
+	GetExitCodeProcess(pi.hProcess, &sum2);
+
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
+
+	// sum1 + sum2
+	_tprintf(_T("%d\n"), sum1 + sum2);
+
+	return 0;
+}
