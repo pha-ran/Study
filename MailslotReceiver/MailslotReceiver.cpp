8200c9b4fb41d2f62ab910d28a043b31ccd6002b
pha-ran <pha-ran@naver.com>
2024-05-07 15:18:28 +0900

    Mailslot Receiver 프로젝트 생성

---
+++ MailslotReceiver/MailslotReceiver.cpp
@@ -0,0 +1,32 @@
+﻿// Mailslot Receiver
+
+#include <windows.h>
+#include <tchar.h>
+#include <stdio.h>
+
+#define MAILSLOT_NAME _T("\\\\.\\mailslot\\test")
+
+int _tmain(void)
+{
+	HANDLE mailSlot;
+	TCHAR message[64];
+	DWORD size;
+
+	mailSlot = CreateMailslot(MAILSLOT_NAME, 0, MAILSLOT_WAIT_FOREVER, NULL);
+
+	for (;;)
+	{
+		ReadFile(mailSlot, message, sizeof(TCHAR) * 64, &size, NULL);
+
+		message[size / sizeof(TCHAR)] = NULL;
+
+		_tprintf(_T("%s\n"), message);
+
+		if (!_tcsncmp(message, _T("exit"), 4))
+			break;
+	}
+
+	CloseHandle(mailSlot);
+
+	return 0;
+}
