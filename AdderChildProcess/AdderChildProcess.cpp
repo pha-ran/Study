b9ce98e37376275efc0d8894150300771692e7df
pha-ran <pha-ran@naver.com>
2024-05-07 19:23:29 +0900

    Adder Child Process 프로젝트 추가

---
+++ AdderChildProcess/AdderChildProcess.cpp
@@ -0,0 +1,20 @@
+﻿// Adder Child Process
+
+#include <windows.h>
+#include <tchar.h>
+#include <stdio.h>
+
+int _tmain(int argc, TCHAR* argv[])
+{
+	DWORD b = _tstoi(argv[1]);
+	DWORD e = _tstoi(argv[2]);
+	DWORD sum = 0;
+
+	while (b <= e)
+	{
+		sum += b;
+		++b;
+	}
+
+	return sum;
+}
