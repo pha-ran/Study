316cd8c087dc52b2c14fb07b943d1beb8a296dc2
pha-ran <pha-ran@naver.com>
2024-05-13 09:53:41 +0900

    Mailslot Sender Child 프로젝트 추가

---
+++ MailslotSenderChild/MailslotSenderChild.cpp
@@ -0,0 +1,29 @@
+﻿// Mailslot Sender Child
+
+#include <windows.h>
+#include <tchar.h>
+#include <stdio.h>
+
+int _tmain(int argc, TCHAR* argv[])
+{
+	HANDLE mailSlot = (HANDLE)_tstoll(argv[1]);
+	TCHAR message[64];
+	DWORD size;
+
+	for (;;)
+	{
+		_tscanf_s(_T("%s"), message, sizeof(TCHAR) * 64);
+
+		WriteFile(
+			mailSlot, message,
+			sizeof(TCHAR) * _tcslen(message), &size, NULL
+		);
+
+		if (!_tcscmp(message, _T("exit")))
+			break;
+	}
+
+	CloseHandle(mailSlot);
+
+	return 0;
+}
